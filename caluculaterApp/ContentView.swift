f95a0ba0103df3fa9a026293feb3f89db6d32a9f
Rent <ren.t21@icloud.com>
2021-11-15 20:51:35 +0900

    Initial Commit

---
+++ caluculaterApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  caluculaterApp
+//
+//  Created by 高橋蓮 on 2021/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ caluculaterApp/caluculaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  caluculaterAppApp.swift
+//  caluculaterApp
+//
+//  Created by 高橋蓮 on 2021/11/15.
+//
+
+import SwiftUI
+
+@main
+struct caluculaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
